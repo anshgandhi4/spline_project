ecbcab09e0ba45021026f2281e045758c0dee5ec
= <ansh.goodboy@gmail.com>
2022-02-19 14:16:32 -0800

    added matplotlib stuff to draw spline

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 def solve(t, v):
@@ -18,15 +19,18 @@ def solve(t, v):
     
     return np.linalg.solve(A, b)
 
-def f(a, x, t):
-    if t[0] <= a < t[1]:
-        return float(x[:4][0] * (a - t[0])**3 + x[:4][1] * (a - t[0])**2 + x[:4][2] * (a - t[0]) + x[:4][3])
-    elif t[1] <= a < t[2]:
-        return float(x[4:8][0] * (a - t[1])**3 + x[4:8][1] * (a - t[1])**2 + x[4:8][2] * (a - t[1]) + x[4:8][3])
-    elif t[2] <= a <= t[3]:
-        return float(x[8:][0] * (a - t[2])**3 + x[8:][1] * (a - t[2])**2 + x[8:][2] * (a - t[2]) + x[8:][3])
-    else:
-        return -1.0
+def f(lst, x, t):
+    output = []
+    for a in lst:
+        if t[0] <= a < t[1]:
+            output.append(float(x[:4][0] * (a - t[0])**3 + x[:4][1] * (a - t[0])**2 + x[:4][2] * (a - t[0]) + x[:4][3]))
+        elif t[1] <= a < t[2]:
+            output.append(float(x[4:8][0] * (a - t[1])**3 + x[4:8][1] * (a - t[1])**2 + x[4:8][2] * (a - t[1]) + x[4:8][3]))
+        elif t[2] <= a <= t[3]:
+            output.append(float(x[8:][0] * (a - t[2])**3 + x[8:][1] * (a - t[2])**2 + x[8:][2] * (a - t[2]) + x[8:][3]))
+        else:
+            output.append(-1.0)
+    return output
 
 def main():
     np.set_printoptions(suppress = True)
@@ -34,8 +38,17 @@ def main():
     t = [0, 3.1, 10.3, 30.1]
     v = [0, 30, 60, 90]
     x = solve(t, v)
-    
-    print(f(0, x, t))
+
+    a = np.linspace(t[0], t[3], 1000)
+    y = f(a, x, t)
+
+    plt.plot(t, v)
+    plt.plot(a, y)
+    plt.scatter(t, v, s = 30)
+
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Velocity (mph)')
+    plt.show()
 
 if __name__ == '__main__':
     main()
